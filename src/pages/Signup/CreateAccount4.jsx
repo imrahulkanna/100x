67cc30d25339d108687a803f79ee717c68a270ff
Rahul Kanna <kannarahul05@gmail.com>
2023-11-18 13:52:48 +0530

    implemented create account step 4 page in modal view

--- src/pages/Signup/CreateAccount4.jsx
+++ src/pages/Signup/CreateAccount4.jsx
@@ -5,6 +5,7 @@ import Fieldset from "../../components/Fieldset";
 import Input from "../../components/Input";
 import Img from "../../components/Img";
 import Button from "../../components/Button";
+import Modal from "../../components/Modal";
 import { useAuth } from "../../context/AuthContext";
 import eyeIcon from "../../assets/visible-t.svg";
 import eyeIconActive from "../../assets/visible-t-blue.svg";
@@ -40,66 +41,68 @@ function CreateAccount4() {
   }
   
   return (
-    <div className="flex min-h-screen flex-shrink-0 flex-col items-start justify-between bg-black px-[15px] pb-5 font-inter text-twitter-neutral-50">
-      <section className="flex flex-col items-start gap-3 self-stretch">
-        {/* Header */}
-        <SignupHeader step={4} />
+    <Modal>
+      <div className="flex h-screen flex-shrink-0 flex-col items-start justify-between bg-black px-[15px] pb-5 font-inter text-twitter-neutral-50 md:h-[475px] md:w-[512px] md:rounded-2xl md:py-5">
+        <section className="flex flex-col items-start gap-3 self-stretch">
+          {/* Header */}
+          <SignupHeader step={4} />
 
-        {/* Adding a Password */}
-        <main className="flex flex-col items-start gap-10 self-stretch">
-          <section className="flex flex-col items-start gap-2 self-stretch">
-            <h1 className="text-2xl font-bold leading-normal">
-              You’ll need a password
-            </h1>
-            <p className="text-sm font-normal leading-normal text-twitter-neutral-500">
-              Make sure it’s 8 characters or more
-            </p>
-          </section>
-          <section className="self-stretch">
-            <Fieldset type={"Password"} isValid={isPasswordValid}>
-              <Input
-                type={type}
-                placeholder="Password"
-                name="Password"
-                value={userCred["Password"]}
-                onChangeHandler={handleChange}
-                onKeyUp={handleKeyUp}
-              />
-              <Img
-                imgPath={icon}
-                imgAlt="visible-t-icon"
-                style="w-6 h-6"
-                onClick={togglePassWordDisplay}
-              />
-            </Fieldset>
-            {!isPasswordValid ? (
-              <p className="text-sm font-medium text-red-600 ">
-                Password must have minimum 8 characters
+          {/* Adding a Password */}
+          <main className="flex flex-col items-start gap-10 self-stretch md:px-7">
+            <section className="flex flex-col items-start gap-2 self-stretch">
+              <h1 className="text-2xl font-bold leading-normal">
+                You’ll need a password
+              </h1>
+              <p className="text-sm font-normal leading-normal text-twitter-neutral-500">
+                Make sure it’s 8 characters or more
               </p>
-            ) : (
-              <></>
-            )}
-          </section>
-        </main>
-      </section>
+            </section>
+            <section className="self-stretch">
+              <Fieldset type={"Password"} isValid={isPasswordValid}>
+                <Input
+                  type={type}
+                  placeholder="Password"
+                  name="Password"
+                  value={userCred["Password"]}
+                  onChangeHandler={handleChange}
+                  onKeyUp={handleKeyUp}
+                />
+                <Img
+                  imgPath={icon}
+                  imgAlt="visible-t-icon"
+                  style="w-6 h-6"
+                  onClick={togglePassWordDisplay}
+                />
+              </Fieldset>
+              {!isPasswordValid ? (
+                <p className="text-sm font-medium text-red-600 ">
+                  Password must have minimum 8 characters
+                </p>
+              ) : (
+                <></>
+              )}
+            </section>
+          </main>
+        </section>
 
-      {/* Next Button */}
-      <section className="self-stretch">
-        {/* <button
+        {/* Next Button */}
+        <section className="self-stretch md:px-5">
+          {/* <button
           className="flex w-full items-center justify-center rounded-signup-radius bg-twitter-neutral-50 px-6 py-3 font-bold leading-normal text-black shadow-signup backdrop-blur-xl-1 hover:bg-twitter-neutral-200"
           onClick={() => navigate("/home")}
         >
           Next
         </button> */}
-        <Button
-          variant="solid"
-          text="Next"
-          color="secondary"
-          onClick={() => navigate("/home")}
-          disabled={userCred["Password"].length < 8}
-        />
-      </section>
-    </div>
+          <Button
+            variant="solid"
+            text="Next"
+            color="secondary"
+            onClick={() => navigate("/home")}
+            disabled={userCred["Password"].length < 8}
+          />
+        </section>
+      </div>
+    </Modal>
   );
 }
 
